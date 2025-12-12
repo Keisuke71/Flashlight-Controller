1df1ba975e2ae87aeda5bcdb7556c3af5b8965c9
Keisuke71 <k.matsumoto071@gmail.com>
2025-12-12 16:35:51 +0900

    add user count

--- server.js
+++ server.js
@@ -3,15 +3,27 @@ const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
-// publicフォルダの中身（HTMLなど）を公開する
 app.use(express.static("public"));
 
-io.on("connection", (socket) => {
-  console.log("ユーザーが接続しました");
+// 接続人数を数える変数
+let connectionCount = 0;
 
-  // スマホから「remote_switch」という命令が来たら
+io.on("connection", (socket) => {
+  // 1. 誰かが来たらカウントアップ
+  connectionCount++;
+  // 全員に「今の人数」を知らせる
+  io.emit("update_count", connectionCount);
+  console.log("接続数:", connectionCount);
+
+  // 2. 誰かがいなくなったらカウントダウン
+  socket.on("disconnect", () => {
+    connectionCount--;
+    // 減った人数を全員に知らせる
+    io.emit("update_count", connectionCount);
+  });
+
+  // 3. ライト操作の命令（今までと同じ）
   socket.on("remote_switch", (status) => {
-    // 全員に「broadcast_switch」として転送する
     io.emit("broadcast_switch", status);
   });
 });
